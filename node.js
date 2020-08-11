a5caf6faf03fb2eeb4ce696a23a025cfe2799e27
Aman Bhagat <bhagataman0399@gmail.com>
2020-08-11 14:08:19 +0530

    Add files via upload
    "Files of contact form in HTML"

---
+++ node.js
@@ -0,0 +1,7 @@
+ function message_submit(){
+    var email = document.getElementById("mail").value;
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+
+    alert (" Your name is "+ fname+" "+lname+" & email is "+email+" Your message is sent Successfully !!!");
+  }
