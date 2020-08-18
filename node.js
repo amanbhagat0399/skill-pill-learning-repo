3836fce0c61c1520958ffdba1424bda4b1a9ee0a
Aman Bhagat <bhagataman0399@gmail.com>
2020-08-18 12:00:20 +0530

    updated contact form with all type of validations

--- node.js
+++ node.js
@@ -2,6 +2,27 @@
     var email = document.getElementById("mail").value;
     var fname = document.getElementById("fname").value;
     var lname = document.getElementById("lname").value;
+    var phone = document.getElementById("phone").value;
 
-    alert (" Your name is "+ fname+" "+lname+" & email is "+email+" Your message is sent Successfully !!!");
+    if(fname ==""){
+      alert("Please enter your Name , Nme can't be blank");
+
+    }
+    else if ((email.indexOf('@') == -1) ||  (email.indexOf(".") == -1 )) {
+        alert("Invalid Email");
+    }
+
+    else if (isNaN(phone)) {
+      alert("Phone number should only contain digits and it must contain 10 digits ");
+    }
+
+    else {
+    alert (" Your name is:-"+ fname+" "+lname+" & email is:-"+email+" Your Contact Number is:-"+phone+"......Your message is sent Successfully...... !!!");
+    }
   }
+
+function lettersonly(input){
+var regex = /[^a-z]/gi;
+input.value = input.value.replace(regex,"");
+
+}
