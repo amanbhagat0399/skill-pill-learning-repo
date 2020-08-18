74ceb9d5147106711b8e60d5d30994f9f06dea11
Aman Bhagat <bhagataman0399@gmail.com>
2020-08-18 14:59:39 +0530

    updated all validation and verification in form

--- node.js
+++ node.js
@@ -8,21 +8,31 @@
       alert("Please enter your Name , Nme can't be blank");
 
     }
-    else if ((email.indexOf('@') == -1) ||  (email.indexOf(".") == -1 )) {
+    else if ((email.indexOf('@') == -1) ||  (email.indexOf(".") == -1 )) 
+    {
         alert("Invalid Email");
     }
 
-    else if (isNaN(phone)) {
+    else if (String(phone).length< 10) 
+    {
       alert("Phone number should only contain digits and it must contain 10 digits ");
     }
 
-    else {
+    else 
+    {
     alert (" Your name is:-"+ fname+" "+lname+" & email is:-"+email+" Your Contact Number is:-"+phone+"......Your message is sent Successfully...... !!!");
     }
   }
 
-function lettersonly(input){
+function lettersonly(input)
+{
 var regex = /[^a-z]/gi;
 input.value = input.value.replace(regex,"");
 
 }
+
+function numbersonly(input)
+  {
+  var regex = /[^0-9]/g ;
+  input.value = input.value.replace(regex,"");
+  }
